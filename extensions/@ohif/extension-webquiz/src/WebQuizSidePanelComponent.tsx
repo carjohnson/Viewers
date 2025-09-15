5c822e33ea721232a4589b4cb41f00e83096020f
BAINESG203\cjohnson <carjohnson@gmail.com>
2025-09-15 11:53:10 -0400

    Cleanup
    Removing commented out code
    Removed display of user and patient info

--- extensions/@ohif/extension-webquiz/src/WebQuizSidePanelComponent.tsx
+++ extensions/@ohif/extension-webquiz/src/WebQuizSidePanelComponent.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-// import { sqrt } from 'math.js'
 import BtnComponent from './components/btnComponent';
-// import { useSystem } from '@ohif/core';
 
 import * as cornerstone from '@cornerstonejs/core';
 import * as cornerstoneTools from '@cornerstonejs/tools';
@@ -11,29 +9,16 @@ import { useStudyInfo } from './hooks/useStudyInfo';
 import { usePatientInfo } from '@ohif/extension-default';
 
 
-// import { RenderingEngine, getRenderingEngine, Enums } from '@cornerstonejs/core';
-
-// const renderingEngineId = 'webquizEngine';
-// const viewportId = 'webquizViewport';
-// const type = Enums.ViewportType.STACK;
-
 
 /**
  *  Creating a React component to be used as a side panel in OHIF.
  *  Also performs a simple div that uses Math.js to output the square root.
  */
-
-
 function WebQuizSidePanelComponent() {
     // set up useEffect hook to manage gathering all data from services
     //  as the other components may be updating asynchronously and this
     //  component needs to be subscribed to those updates
 
-    // const { servicesManager } = useSystem();
-    // const { segmentationService } = servicesManager.services;
-    
-    // const [segmentationData, setSegmentationData] = useState([]);
-    // const [volumeData, setVolumeData] = useState([]);
     const [annotationData, setAnnotationData] = useState([]);
     const [userInfo, setUserInfo] = useState(null);
     const [isSaved, setIsSaved] = useState(true);
@@ -76,45 +61,12 @@ function WebQuizSidePanelComponent() {
     // console.log('📦 Zustand store currently holds:', studyInfo);
     
 
-    // // for render engine, 
-    // const cornerstoneRef = useRef<HTMLDivElement>(null);
-    // const [engine, setEngine] = useState<RenderingEngine | null>(null);
-
-    // useEffect(() => {
-    //     const element = cornerstoneRef.current;
-    //     if (!element) return;
-
-    //     let renderingEngine = getRenderingEngine(renderingEngineId);
-    //     if (!renderingEngine) {
-    //     renderingEngine = new RenderingEngine(renderingEngineId);
-    //     renderingEngine.enableElement({
-    //         viewportId,
-    //         element,
-    //         type,
-    //     });
-    //     renderingEngine.renderViewports([viewportId]);
-    //     }
-
-    //     setEngine(renderingEngine);
-    // }, []);
-
-
-
     type AnnotationStats = Record<string, Record<string, unknown>>;     // generic for capture of cachedStats object
     // Annotations listeners
     useEffect(() => {
         const handleAnnotationChange = () => {
             const lo_annotationStats = getAnnotationsStats();
             setAnnotationData(lo_annotationStats);
-
-            ////// for debug /////
-            // const lengths = lo_annotationStats.flatMap((statsObj) => 
-            //     Object.values(statsObj)
-            //         .filter((stat): stat is { length: number } => typeof stat === 'object' && stat !== null && 'length' in stat)
-            //         .map((stat) => stat.length)
-            //     );
-
-            // console.log("LENGTHS ===>", lengths);
         } ;
 
         // Register listeners
@@ -133,64 +85,6 @@ function WebQuizSidePanelComponent() {
         }
     }, [] );
 
-    //=====================
-    // Segmentation listener
-
-
-    // // don't rely on segmentationService. 
-    // // These useEffects are tapping into the events for a more immediate response
-    // // useEffect(() => {
-    // //     const lo_allVolumes = getSegmentationStats();
-    // //     setVolumeData(lo_allVolumes);
-    // //     console.table(lo_allVolumes);
-    // //     }, [segmentationService]);
-    // // Refactored ... ===>
-    // useEffect(() => {
-    //     const handleSegmentationChange = () => {
-    //         const [lo_segmentations, lo_allVolumes] = getSegmentationStats();
-    //         setVolumeData(lo_allVolumes);
-    //         console.table(lo_allVolumes);
-    //     };
-
-    //     cornerstone.eventTarget.addEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_ADDED,
-    //         handleSegmentationChange
-    //     );
-
-    //     cornerstone.eventTarget.addEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_DELETED,
-    //         handleSegmentationChange
-    //     );
-
-    //     cornerstone.eventTarget.addEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_MODIFIED,
-    //         handleSegmentationChange
-    //     );
-
-    //     cornerstone.eventTarget.addEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_DATA_MODIFIED,
-    //         handleSegmentationChange
-    //     );
-
-    //     return () => {
-    //         cornerstone.eventTarget.removeEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_ADDED,
-    //         handleSegmentationChange
-    //         );
-    //         cornerstone.eventTarget.removeEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_DELETED,
-    //         handleSegmentationChange
-    //         );
-    //         cornerstone.eventTarget.removeEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_MODIFIED,
-    //         handleSegmentationChange
-    //         );
-    //         cornerstone.eventTarget.removeEventListener(
-    //         cornerstoneTools.Enums.Events.SEGMENTATION_DATA_MODIFIED,
-    //         handleSegmentationChange
-    //         );
-    //     };
-    // }, []);
 
     //=====================
     // watch for changes to the state properties
@@ -201,19 +95,6 @@ function WebQuizSidePanelComponent() {
         }
     }, [annotationData]);
 
-    // useEffect(() => {
-    //     if (volumeData.length > 0) {
-    //         setIsSaved(false)
-    //         // console.log(' Volume Change');
-    //     }
-    // }, [volumeData]);
-
-    // useEffect(() => {
-    //     if (segmentationData.length > 0) {
-    //         setIsSaved(false)
-    //         // console.log(' Volume Change');
-    //     }
-    // }, [segmentationData]);
 
     // get user info from parent iframehost
     useEffect(() => {
@@ -260,43 +141,12 @@ function WebQuizSidePanelComponent() {
         return lo_annotationStats;
     };
 
-    // //=====================
-    // // function to get the list of objects holding segmentations and
-    // //  extract volume data
-    // const getSegmentationStats = () => {
-    //     const lo_segmentations = segmentationService.getSegmentations();
-    //     const lo_allVolumes = [];
-
-    //     lo_segmentations.forEach((segmentation, segIndex) => {
-    //         const segments = segmentation.segments;
-
-    //         Object.keys(segments).forEach((segmentKey) => {
-    //         const segment = segments[segmentKey];
-    //         const volume = segment?.cachedStats?.namedStats?.volume?.value;
-
-    //         if (volume !== undefined) {
-    //             lo_allVolumes.push({
-    //             segmentation: segIndex + 1,
-    //             segment: segmentKey,
-    //             volume,
-    //             });
-    //         }
-    //         });
-    //     });
-    //     return [lo_segmentations, lo_allVolumes];
-    // };
 
     //=====================
     const refreshData = () => {
         const lo_annotationStats = getAnnotationsStats();
         setAnnotationData(lo_annotationStats);
         
-        // const [lo_segmentations, lo_allVolumes] = getSegmentationStats();
-        // setVolumeData(lo_allVolumes);
-        // setSegmentationData(lo_segmentations);
-        // console.table(lo_allVolumes);
-
-        // return [lo_annotationStats, lo_allVolumes, lo_segmentations]; // ensures stats are updated before continuing
         return [lo_annotationStats]; // ensures stats are updated before continuing
     };
 
@@ -313,7 +163,7 @@ function WebQuizSidePanelComponent() {
             setIsSaved={setIsSaved}
             studyInfo={studyInfo}
         />
-        {userInfo && (
+        {/* {userInfo && (
             <div>
                 <div>User Name: {userInfo.username}</div>
                 <div>User Role: {userInfo.role}</div>
@@ -324,7 +174,7 @@ function WebQuizSidePanelComponent() {
                 <div>Patient Name: {studyInfo.patientName}</div>
                 <div>StudyUID: {studyInfo.studyUID}</div>
             </div>
-        )}
+        )} */}
         </div>
     );    
 
--- extensions/@ohif/extension-webquiz/src/components/btnComponent.tsx
+++ extensions/@ohif/extension-webquiz/src/components/btnComponent.tsx
@@ -1,37 +1,22 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { Button } from '@ohif/ui-next';
-import { data } from 'dcmjs';
-// import { Buffer } from 'buffer';
-// import { getGeneratedSegmentation } from "../utils/util_segmentation";
+// import { data } from 'dcmjs';
 import { annotation } from '@cornerstonejs/tools';
 
-
-/////////////
-// NOTE:
-// Removing customized rendering engine since AddAnnotation already does the render
-// There may be some missing variables at this stage if this is to
-// be brought back on line
-// import { RenderingEngine } from '@cornerstonejs/core';
-
-
-const { datasetToDict } = data;
+// const { datasetToDict } = data;
 
 interface BtnComponentProps {
   userInfo: any;
   refreshData: () => void;
   setIsSaved: (value: boolean) => void;
   studyInfo: any;
-  // renderingEngine: RenderingEngine | null;
 }
-
-
 
 const BtnComponent: React.FC<BtnComponentProps> = ( {
   userInfo,
   refreshData,
   setIsSaved,
   studyInfo
-  // renderingEngine
 }) => {
  
   const [listOfUsersAnnotations, setListOfUsersAnnotations] = useState(null);
@@ -40,24 +25,11 @@ const BtnComponent: React.FC<BtnComponentProps> = ( {
 
   const userDefinedBtnLabel = () => {
     if (userInfo?.role === "admin") {
-      return "Restore all users' measurements.";
+      return "Restore all users' measurements";
     } else {
-      return "Restore measurements.";
+      return "Restore measurements";
     }
   };
-
-  //>>>>> for rendering engine <<<<<
-  // const { servicesManager } = useSystem();
-  // const displaySetService = servicesManager.services.displaySetService;
-  // const activeDisplaySets = displaySetService.getActiveDisplaySets();
-
-  // const studyInstanceUID = activeDisplaySets[0]?.StudyInstanceUID;
-  // const { patientInfo } = usePatientInfo();
-  // // in this study, patient name is set up to be unique and is being
-  // //  sent to the parent as the patient_id
-  // const patientName = patientInfo.PatientName;
-  //>>>>> <<<<<
-
 
   const handleUploadAnnotationsClick = () => {
 
@@ -67,10 +39,7 @@ const BtnComponent: React.FC<BtnComponentProps> = ( {
     const allAnnotations = annotation.state.getAllAnnotations();
     measurementListRef.current = [...allAnnotations];
     
-    // console.log('Number of measurements: ', freshMeasurementData.length);
-    // console.log("Number of segments:", freshVolumeData.length)
     console.log("Number of annotation objects:", measurementListRef.current.length)
-
 
     window.parent.postMessage({
       type: 'annotations', 
@@ -81,79 +50,6 @@ const BtnComponent: React.FC<BtnComponentProps> = ( {
     }, '*');
     setIsSaved(true);
   }
-
-
-  // const handleUploadSegmentationsClick = async () => {
-  //   const [, , freshSegmentationData] = refreshData();
-  //   const selectedId = freshSegmentationData[0]?.segmentationId;
-
-  //   const viewportService = servicesManager.services.cornerstoneViewportService;
-  //   const viewportIds = viewportService.getViewportIds();
-
-  //   const state = { segmentationId: selectedId, viewportIds };
-  //   const result = await getGeneratedSegmentation(state);
-
-  //   if (!result?.dataset) {
-  //     console.error("No dataset generated.");
-  //     return;
-  //   }
-
-  //   uploadDICOMData(result.dataset, `segmentation-${Date.now()}.dcm`);
-  // };
-
-  // const uploadDICOMData = ( dataset, filename )  => {
-  //   try {
-  //     const buffer = Buffer.from(datasetToDict(dataset).write());
-  //     const blob = new Blob([buffer], { type: "application/dicom" });
-
-  //       // Send blob to parent app
-  //     window.parent.postMessage(
-  //       {
-  //         type: "SEGMENTATION_UPLOAD",
-  //         filename,
-  //         payload: blob
-  //       },
-  //       "*"
-  //     );
-
-  //     console.log("📤 Segmentation message posted to parent window.");
-  //   } catch (error) {
-  //     console.error("❌ Failed to post segmentation:", error);
-  //   }
-
-  // }
-
-
-  // const saveAnnotations = () => {
-  //   const allAnnotations = annotation.state.getAllAnnotations();
-  //   measurementListRef.current = [...allAnnotations];
-  // };
-
-  // const handleClearMeasurementsClick = async () => {
-  //   saveAnnotations();
-  //   console.log("Clearing measurements");
-  //   measurementListRef.current.forEach(annotationEntry => {
-  //     const { annotationUID } = annotationEntry;
-  //     annotation.state.removeAnnotation(annotationUID);
-  //   });
-
-  //   // triggerRender();
-  // };
-
-
-  // const handleRedrawSavedMeasurementsClick = () => {
-  //   measurementListRef.current.forEach(savedAnnotation => {
-  //     if (
-  //       savedAnnotation &&
-  //       typeof savedAnnotation.annotationUID === 'string' &&
-  //       savedAnnotation.annotationUID.length > 0
-  //     ) {
-  //       annotation.state.addAnnotation(savedAnnotation);
-  //     }
-  //   });
-
-  // };
-
 
 //   // useEffect if you want the annotations to appear automatically when the component mounts
 //   useEffect(() => {
@@ -227,24 +123,6 @@ const BtnComponent: React.FC<BtnComponentProps> = ( {
       console.error('❌ Error fetching annotations:', error);
     }
   }
-
-
-
-// const triggerRender = () => {
-
-//   if (!renderingEngine) {
-//     console.error('❌ Rendering engine not found. Did you initialize it?');
-//     return;
-//   }
-
-//   const viewportService = servicesManager.services.cornerstoneViewportService;
-//   const viewportIds = viewportService.getViewportIds();
-
-//   if (viewportIds.length > 0) {
-//     renderingEngine.renderViewports(viewportIds);
-//   }
-// };
-
   
   return (
       <div>
